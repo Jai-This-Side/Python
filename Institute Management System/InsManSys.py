268449886c40562c6b76918f71cd9760c5951cb7
Jai-This-Side <jaisinghprsnl99@gmail.com>
2025-06-02 03:50:39 +0530

    modified code

--- Institute Management System/InsManSys.py
+++ Institute Management System/InsManSys.py
@@ -1,4 +1,4 @@
-print("\t\t\t\t\t ~~~~ My Institute ~~~~")
+print("\t\t\t\t\t ~~~~ Career Path ~~~~")
 print("\t\tPress 1 for Admission enquiry")
 print("\t\tPress 2 for Employee details")
 print("\t\tPress 3 for student details")
@@ -19,10 +19,10 @@ if Choice_1 == 1 :
       course_list = file_lang.split('\n')
 
    if New_course in course_list:
-      print("course available! contact us through our contact number 966766###4 or email us at imarticus@outlook.com")
+      print("course available! contact us through our contact number 966766###4 or email us at careerpath@outlook.com")
 
    else:
-      print("Sorry to say but the course is not available at this moment")
+      print("Sorry but the course is not available at this moment")
 
 password = "aman01"
 
@@ -65,9 +65,9 @@ if Choice_1 == 3:
       StudentMatch = StudentDetails.split('\n')
 
    if name in StudentMatch:
-      print("The student is from Imarticus Mega. What do you want to know about him?")
+      print("The student is from Career Path. What do you want to know about him?")
 
    else:
-      print("Sorry the student with the name provided does not belong to Imarticus Mega")
+      print("Sorry the student with the provided name does not belong to Career Path")
 
    
